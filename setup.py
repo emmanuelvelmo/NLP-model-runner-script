ba0a79898b2a4e36282e7555752b67ad621245c4
Emmanuel V. <51292782+emmanuelvelmo@users.noreply.github.com>
2025-08-08 15:40:34 -0700

    setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from cx_Freeze import setup, Executable
 
-setup(name="NLP model runner script", executables=[Executable("NLP model runner script.py")], options={"build_exe": {"excludes": ["tkinter"]}})
+setup(name="NLP model runner", executables=[Executable("NLP model runner script.py")], options={"build_exe": {"excludes": ["tkinter"]}})
